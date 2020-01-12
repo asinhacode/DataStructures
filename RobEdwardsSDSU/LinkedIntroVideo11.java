aa3e3b2e975ac6fff35993ef16c7d3b25f921617
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-01-12 02:04:57 -0500

    introduction

---
+++ RobEdwardsSDSU/LinkedIntroVideo11.java
@@ -0,0 +1,49 @@
+package linkedList;
+
+public class LinkedList<E> 
+{
+	// private class of LinkedList
+	/*  A description of a node
+	 * 			__________
+				|		         | 
+				|	   data    |
+				|		         |
+				|		         |
+				| __________ |		   
+				|		         |
+				|		         |
+				|	   next    |
+				|		         |
+				| ___________|		   
+				
+	 */
+	class Node<E>
+	{
+		E data;
+		Node<E> next;
+		
+		public Node(E obj) // constructor to create a new node
+		{
+			data = obj;
+			next = null;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	private Node<E> head;   // pointer
+	private int sizeOfList; // keeping track of the list
+	
+	public LinkedList() // constructor to create a chain
+	{
+		head = null;
+		sizeOfList = 0;
+	}
+	
+}
+
